dc9b792c89afa3a270859885386692e5e6f8f26a
Josef Rousek <josef.rousek@gmail.com>
2016-11-05 12:27:44 +0100

    Fixed #27370 -- Prevented Select from using 'required' with a non-empty first value.

--- django/forms/widgets.py
+++ django/forms/widgets.py
@@ -706,6 +706,27 @@ class Select(ChoiceWidget):
             context['widget']['attrs']['multiple'] = 'multiple'
         return context
 
+    def use_required_attribute(self, initial):
+        """
+        Don't render 'required' if the first <option> has a value, as that's
+        invalid HTML.
+        """
+        use_required_attribute = super(Select, self).use_required_attribute(initial)
+        # 'required' is always okay for <select multiple>.
+        if self.allow_multiple_selected:
+            return use_required_attribute
+
+        def choice_has_empty_value(choice):
+            """Return True if the choice's value is empty string or None."""
+            value, _ = choice
+            return (
+                (isinstance(value, six.string_types) and not bool(value)) or
+                value is None
+            )
+
+        first_choice = next(iter(self.choices), None)
+        return use_required_attribute and first_choice is not None and choice_has_empty_value(first_choice)
+
 
 class NullBooleanSelect(Select):
     """
